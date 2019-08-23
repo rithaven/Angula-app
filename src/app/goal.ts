9d49550cf0c70af5a16ab128aff15684ec1fe3cc
RITHA <veneruth4@gmail.com>
2019-08-23 17:02:12 +0200

    ok

---
+++ src/app/goal.ts
@@ -0,0 +1,12 @@
+// export class Goal {
+//     showDescription: boolean;
+//     constructor(public id: number,public name: string,public description: string){
+//       this.showDescription=false;
+//     }
+//   }
+export class Goal {
+    public showDescription: boolean;
+    constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+      this.showDescription=false;
+    }
+  }
